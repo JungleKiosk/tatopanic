4209ca7f0b13bde61f21b6aa10309b3ad14dbead
Fra <135766845+JungleKiosk@users.noreply.github.com>
2025-02-28 19:20:33 +0100

    scss - colormode refact

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,38 @@
 <script>
 
+export default {
+    name: "about",
+
+};
 </script>
 
-
 <template>
-    <div class="container">
-        <div class="row mt-5 justify-content-center">
-            <div class="col-12 col-lg-8 p-3">
-                <h1 class="display-1">{{ $t('about.title') }}</h1>
-                <br>
+    <div>
+        <header class="header">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-8 p-3">
+                        <div class="jumbotron p-5 rounded-3">
+                            <h1 class="display-1">{{ $t('about.title') }}</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </header>
+
+        <main class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-8 p-3">
+                    <h2>{{ $t('about.subtitle') }}</h2>
+                    <p>{{ $t('about.description') }}</p>
+                </div>
+            </div>
+        </main>
+
+        <Footer></Footer>
     </div>
 </template>
 
+<style>
 
-
-<style scoped>
-/* Aggiungi eventuali stili */
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,39 @@
 <script>
 
-
 export default {
-    /* components: { BiogasInfo }, */
-    name: "Home",
+    name: "home",
 
 };
 </script>
 
 
 <template>
-    <div class="container">
-        <div class="row mt-5 justify-content-center">
-            <div class="col-12 col-lg-8 p-3">
-                <h1 class="display-1">{{ $t('home.title') }}</h1>
-                <br>
+    <div>
+        <header class="header">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-12 col-lg-8 p-3">
+                        <div class="jumbotron p-5 rounded-3">
+                            <h1 class="display-1">{{ $t('home.title') }}</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </header>
+
+        <main class="container mt-5">
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-8 p-3">
+                    <h2>{{ $t('home.subtitle') }}</h2>
+                    <p>{{ $t('home.description') }}</p>
+                </div>
+            </div>
+        </main>
+
+        <Footer></Footer>
     </div>
 </template>
 
-
-
 <style scoped>
-/* Aggiungi eventuali stili */
+
 </style>
