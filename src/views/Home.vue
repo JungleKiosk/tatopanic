e7b8ad21fa82ad0cc5d984767336472fa9dbd7b7
Fra <135766845+JungleKiosk@users.noreply.github.com>
2025-02-28 23:13:31 +0100

    duble wave🚩

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,20 +14,25 @@ export default {
 <template>
     <div>
         <header class="header pt-5">
-
-            <Jumbo></Jumbo>
+            <div class="row justify-content-center">
+                <div class="col-12 col-lg-4">
+                    <div class="jumbotron rounded-3">
+                        <h1 class="display-1">{{ $t('home.title') }}</h1>
+                        <!-- !--Waves Container -->
+                        <!--Waves end-->
+                    </div>
+                </div>
+            </div>
             <!-- !--Waves Container -->
             <div>
+                <!-- Onda superiore -->
                 <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
                     viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
                     <defs>
-                        <!-- Gradiente Azzurro -->
                         <linearGradient id="waveGradient" x1="0%" y1="0%" x2="0%" y2="100%">
                             <stop offset="0%" style="stop-color:#6EC6FF; stop-opacity:1" />
                             <stop offset="100%" style="stop-color:#1E90FF; stop-opacity:1" />
                         </linearGradient>
-
-                        <!-- Percorso dell'onda -->
                         <path id="gentle-wave"
                             d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
                     </defs>
@@ -39,12 +44,22 @@ export default {
                     </g>
                 </svg>
             </div>
-
-
-
         </header>
 
+        <!-- Onda inferiore specchiata -->
+        <svg class="waves flipped" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <g class="parallax">
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="url(#waveGradient)" opacity="0.7" />
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="url(#waveGradient)" opacity="0.5" />
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="url(#waveGradient)" opacity="0.3" />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="url(#waveGradient)" opacity="1" />
+            </g>
+        </svg>
+        
         <main class="container mt-5">
+
+            <Jumbo></Jumbo>
             <div class="row justify-content-center">
                 <div class="col-12 col-lg-8 p-3">
                     <h2>{{ $t('home.subtitle') }}</h2>
